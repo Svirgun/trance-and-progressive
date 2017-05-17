ffd6598ab57fcd4fe82cf85317a1a9d96908a228
Zinovij Svirgun <svirgunzinovij@gmail.com>
2017-05-17 10:52:11 +0000

    Initial

---
+++ js/index.js
@@ -0,0 +1,24 @@
+// $(document).ready(function() {
+// $('a[href^="#"]').click(function(){
+// var el = $(this).attr('href');
+// $('body').animate({
+// scrollTop: $(el).offset().top}, 2000);
+// return false;
+// });
+// });
+
+$(document).ready(function(){
+	$("#menu").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+});
